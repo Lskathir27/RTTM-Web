e29d447ee8809a5edd43882b760c0796af2899de
Kathirvelu M <kathirvelu.murugan@digitalsoft.com>
2024-03-14 12:25:34 +0530

    Added side nav and Theming functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, HostBinding, Renderer2 } from '@angular/core';
+import { RouterOutlet } from '@angular/router'
+import { MaterialModule } from '../Shared/Modules/Material/material.module';
+import { ApplicationTheme } from './const';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MaterialModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  appTheme = ApplicationTheme;
+  theme = this.appTheme.DARK;
+  showLabel: boolean = false;
   title = 'RealTimeTaskManagement';
+  isDarkMode: boolean = false;
+
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer,
+    private renderer: Renderer2
+  ) {
+    matIconRegistry.addSvgIcon(
+      'rttm',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/AppIcon/rttm.svg')
+    )
+  }
+
+  switchTheme() {
+    this.handleClassChange()
+  }
+
+  handleClassChange() {
+    this.isDarkMode ?
+      this.renderer.addClass(document.body, this.appTheme.DARK) :
+        this.renderer.removeClass(document.body, this.appTheme.DARK);
+  }
 }
---
+++ src/app/const.ts
@@ -0,0 +1,4 @@
+export enum ApplicationTheme {
+  LIGHT = 'light-theme',
+  DARK = 'dark-theme'
+}
